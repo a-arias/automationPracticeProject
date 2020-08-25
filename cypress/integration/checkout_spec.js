43a71596a0204152b0843cfa74f42d51daf68cdf
arias <aarias@ariass-MacBook-Pro.local>
2020-08-24 18:27:48 -0700

    increasing test coverage for checkout spec

---
+++ cypress/integration/checkout_spec.js
@@ -0,0 +1,30 @@
+context('Checkout Functionality', () => {
+  beforeEach(() => {
+    cy.loginAsNormalUser();
+  });
+
+  it('should be able to buy a new woman given dress', () => {
+    let dress = 'Printed Summer Dress'
+
+    // # Clicks womens link
+    cy.get(".sf-with-ul").contains("Women").click();
+
+    //# searching for given dress to buy
+    cy.get("a.product-name").contains(dress).click();
+    
+    //# adding item to the cart
+    cy.get("#add_to_cart").click();
+
+    //# clicking all checkout buttons 
+    cy.clickCheckoutAllsectionsButtons();
+
+    //# Click pay using Check
+    cy.get("a[title='Pay by check.']").click();
+
+    //# Click confirm order button
+    cy.get("button[type='submit']").contains('I confirm my order').click();
+
+    //# Assert the order was successfully made
+    cy.get("p.alert-success").contains('Your order on My Store is complete.').should('be.visible') 
+  });
+});
